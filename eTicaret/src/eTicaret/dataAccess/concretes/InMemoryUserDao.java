7670fc13f547a28d03379da04845a7b29c90758f
Hp <kamranhesimov9@gmail.com>
2021-05-08 06:20:53 +0400

    Ödev Tamamlandı

--- eTicaret/src/eTicaret/dataAccess/concretes/InMemoryUserDao.java
+++ eTicaret/src/eTicaret/dataAccess/concretes/InMemoryUserDao.java
@@ -1,11 +1,23 @@
 package eTicaret.dataAccess.concretes;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import eTicaret.dataAccess.abstracts.UserDao;
 import eTicaret.entities.concretes.User;
 
 public class InMemoryUserDao implements UserDao {
+
+	private List<User> userArrayList = new ArrayList<User>();
+
+	public InMemoryUserDao() {
+		System.out.println("==================== Kullanıcılar ====================");
+		User user1 = new User(1, "Kamran", "Hashimov", "email@gmail.com", "19khKH#");
+		User user2 = new User(2, "Engin", "Demirog", "engindemirog34@gmail.com", "123edED@");
+
+		userArrayList.add(user1);
+		userArrayList.add(user2);
+	}
 
 	@Override
 	public void add(User user) {
@@ -26,12 +38,18 @@ public class InMemoryUserDao implements UserDao {
 
 	@Override
 	public User get(int id) {
-		return null;
+		User user = userArrayList.stream().filter(u -> u.getId() == id).findFirst().orElse(null);
+		System.out.println("Kullanıcı adı: " + user.getFirstName());
+		return user;
 	}
 
 	@Override
 	public List<User> getAll() {
-		return null;
+		for (User user : userArrayList) {
+			System.out.println(user.getFirstName());
+		}
+		System.out.println("================================");
+		return this.userArrayList;
 	}
 
 	@Override
